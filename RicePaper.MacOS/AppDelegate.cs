2bf5c4cb944fa660f9d20028763e1e067bfffed0
Mullen <fmullen@illumina.com>
2020-01-16 22:12:41 -0800

    Working tray with no design

---
+++ RicePaper.MacOS/AppDelegate.cs
@@ -0,0 +1,57 @@
+﻿using AppKit;
+using Foundation;
+
+namespace RicePaper.MacOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        #region Properties
+        private readonly NSStatusItem statusItem;
+        #endregion
+
+        #region Constructor
+        public AppDelegate()
+        {
+            statusItem = NSStatusBar.SystemStatusBar.CreateStatusItem(NSStatusItemLength.Variable);
+        }
+        #endregion
+
+        #region NS Lifecycle
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            statusItem.Title = "Ω";
+            statusItem.Button.Target = this;
+            //statusItem.Button.Image = NSImage.ImageNamed("");
+            statusItem.Button.Action = new ObjCRuntime.Selector("StatusBarClicked:");
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+        #endregion
+
+        #region Actions
+        [Action("StatusBarClicked:")]
+        public void StatusBarClicked(NSObject sender)
+        {
+            try
+            {
+                var storyboard = NSStoryboard.FromName("Main", null);
+                var controller = storyboard.InstantiateControllerWithIdentifier("ViewController") as ViewController;
+
+                var popoverView = new NSPopover();
+                popoverView.ContentViewController = controller;
+                popoverView.Behavior = NSPopoverBehavior.Transient;
+                popoverView.Show(statusItem.Button.Bounds, statusItem.Button, NSRectEdge.MaxYEdge);
+
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+        #endregion
+    }
+}
---
+++ RicePaper.MacOS/ViewController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Policy;
+using AppKit;
+using Foundation;
+
+namespace RicePaper.MacOS
+{
+    public partial class ViewController : NSViewController
+    {
+        #region Properties
+        public override NSObject RepresentedObject
+        {
+            get { return base.RepresentedObject; }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ViewController(IntPtr handle) : base(handle) { }
+        #endregion
+
+        #region NS Lifecycle
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            FilePathLabel.StringValue = "TO BE DETERMINED";
+        }
+        #endregion
+
+        #region Action Handlers
+        partial void FileDialogButton(NSButton sender)
+        {
+            try
+            {
+                var dialog = NSOpenPanel.OpenPanel;
+                dialog.CanChooseFiles = false;
+                dialog.CanChooseDirectories = true;
+                dialog.Title = "Select directory";
+                dialog.ShowsResizeIndicator = true;
+                dialog.ShowsHiddenFiles = false;
+                dialog.AllowsMultipleSelection = false;
+
+                var outcome = dialog.RunModal();
+                if (outcome == 1)
+                {
+                    var result = dialog.Url;
+
+                    if (result != null)
+                    {
+                        var path = result.Path;
+                        FilePathLabel.StringValue = path;
+                    }
+                }
+                else
+                {
+                    // was canceled
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
